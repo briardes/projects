9b82043f9975a9c1227baec5f6affa306ac7201c
briardes <brian@Brians-MacBook-Air.local>
2014-06-03 03:36:03 -0400

    Added 5 guess limit to guessing game

--- guess.rb
+++ guess.rb
@@ -1,13 +1,17 @@
 randnum = rand(100)
 print 'Pick a number!'
 guess = nil
-while guess != randnum
+count = 5
+while (guess != randnum) && (count >= 0)
   guess = gets.chomp.to_i
   if guess == randnum
     puts 'Great Job!'
   elsif guess > randnum then
     puts 'Pick a lower number.'
+    count -= 1
   else
     puts 'Try something higher'
+    count -= 1
   end
 end
+puts 'Game Over!'
