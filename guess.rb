ed3524a19cedbff8695ae3ba6aa88b1a7958f213
briardes <brian@Brians-MacBook-Air.local>
2014-06-04 03:15:04 -0400

    Changed format of code to use methods, added chance count and end game message including cp number

--- guess.rb
+++ guess.rb
@@ -1,17 +1,36 @@
-randnum = rand(100)
-print 'Pick a number!'
-guess = nil
-count = 5
-while (guess != randnum) && (count >= 0)
-  guess = gets.chomp.to_i
-  if guess == randnum
-    puts 'Great Job!'
-  elsif guess > randnum then
-    puts 'Pick a lower number.'
-    count -= 1
-  else
-    puts 'Try something higher'
-    count -= 1
+class Game
+  def new_guess
+    print 'Guess a number 1-100: '
+    @guess = gets.chomp.to_i
+  end
+
+  def new_rand
+    @randnum = rand(1..100)
+  end
+
+  def feedback
+    if @guess == @randnum
+      puts "Great job! You finished with #{@count} chances left."
+    elsif @guess > @randnum && @count >= 1
+      puts "Too high, try again. You have #{@count} chances left."
+    elsif @count >= 1
+      puts "Too low, try again. You have #{@count} chances left."
+    end
+  end
+
+  def set_count
+    @count = 4
+  end
+
+  def run
+    new_rand
+    set_count
+    while @guess != @randnum && @count >= 0
+      new_guess
+      feedback
+      @count -= 1
+    end
+    puts "Game over, the answer was #{@randnum}." if @guess != @randnum
   end
 end
-puts 'Game Over!'
+Game.new.run
